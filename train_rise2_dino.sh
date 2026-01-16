911d0077617fabdac190a73711fdd636575d8e15
Qin Haoxiang <qhx_sjtu@sjtu.edu.cn>
2026-01-16 09:54:58 +0800

    failed again!

--- train_rise2_dino.sh
+++ train_rise2_dino.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
-export CUDA_VISIBLE_DEVICES=1,3
-torchrun --master_addr 127.0.0.1 --master_port 23334 --nproc_per_node 2 --nnodes 1 --node_rank 0 train.py --data_path /data/haoxiang/realdata_rise2_ready --ckpt_dir /data/haoxiang/logs/rise2_2601 --config configs/single_rise1.yaml
+export CUDA_VISIBLE_DEVICES=3
+torchrun --master_addr 127.0.0.1 --master_port 23334 --nproc_per_node 1 --nnodes 1 --node_rank 0 train.py --data_path /data/haoxiang/realdata_rise2_ready --ckpt_dir /data/haoxiang/logs/rise2_2601 --config configs/single_rise1.yaml
 
