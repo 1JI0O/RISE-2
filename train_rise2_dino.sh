f25d3cfee3a01ce0c0f0541fafc26a02b01b172b
Qin Haoxiang <qhx_sjtu@sjtu.edu.cn>
2026-01-17 15:32:11 +0800

    finish train

--- train_rise2_dino.sh
+++ train_rise2_dino.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
-export CUDA_VISIBLE_DEVICES=3
+export CUDA_VISIBLE_DEVICES=1,3
 export OMP_NUM_THREADS=1
 export MKL_NUM_THREADS=1
-torchrun --master_addr 127.0.0.1 --master_port 23334 --nproc_per_node 1 --nnodes 1 --node_rank 0 train.py --data_path /data/haoxiang/realdata_rise2_ready --ckpt_dir /data/haoxiang/logs/rise2_2601 --config configs/single_rise1.yaml
+torchrun --master_addr 127.0.0.1 --master_port 23334 --nproc_per_node 2 --nnodes 1 --node_rank 0 train.py --data_path /data/haoxiang/realdata_rise2_ready --ckpt_dir /data/haoxiang/logs/rise2_2601 --config configs/single_rise1.yaml --resume_ckpt /data/haoxiang/logs/rise2_2601/policy_step_12500_seed_233.ckpt --resume_step 12500
 
