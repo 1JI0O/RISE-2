25bfc419297a2a234a09478756cafb1a4ec5f159
Qin Haoxiang <qhx_sjtu@sjtu.edu.cn>
2026-03-05 20:52:42 +0800

    update

--- command_train_3donly.sh
+++ command_train_3donly.sh
@@ -14,3 +14,4 @@
 # CUDA_VISIBLE_DEVICES=0 torchrun --master_addr 127.0.0.1 --master_port 23333 --nproc_per_node 1 --nnodes 1 --node_rank 0 train.py --data_path /data/haoxiang/data/airexo2/task_0013 --ckpt_dir /data/haoxiang/logs/airexo_mask_task0013 --config configs/dual_teleop_dino.yaml
 
 CUDA_VISIBLE_DEVICES=0,1,2,3 torchrun --master_addr 127.0.0.1 --master_port 23333 --nproc_per_node 4 --nnodes 1 --node_rank 0 train_3donly.py --data_path /data/haoxiang/data/airexo2/task_0013 --ckpt_dir /data/haoxiang/logs/airexo_mask_task0013_3donly --config configs/dual_teleop_dino_3donly.yaml
+
